28868db8fbe578ad0ef7b65a6912a7ab16b614eb
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-21 15:11:34 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "M", "Pink", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Purple", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 11, "M", "Blue", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "Red", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "Black", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "Pink", "happy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, null, null, null, null, null);
+
